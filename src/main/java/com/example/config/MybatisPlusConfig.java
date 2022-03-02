85fac6d0d8c85a9a3389fd0d4e35b291ff8d784f
泽兔 <hunter321@dingtalk.com>
2022-03-02 18:23:44 +0800

    init

--- src/main/java/com/example/config/MybatisPlusConfig.java
+++ src/main/java/com/example/config/MybatisPlusConfig.java
@@ -41,17 +41,4 @@ public class MybatisPlusConfig {
     }
 
 
-    /**
-     * 相当于顶部的：
-     * {@code @MapperScan("com.baomidou.springboot.mapper*")}
-     * 这里可以扩展，比如使用配置文件来配置扫描Mapper的路径
-     */
-//    @Bean
-//    public MapperScannerConfigurer mapperScannerConfigurer(){
-//        MapperScannerConfigurer scannerConfigurer = new MapperScannerConfigurer();
-//        scannerConfigurer.setBasePackage("com.ops.dao.mapper*");
-//        return scannerConfigurer;
-//    }
-
-
 }
